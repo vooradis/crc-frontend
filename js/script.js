d524504929197b726e588e413afd17c194561e8f
Samrat Vooradi <106648385+vooradis@users.noreply.github.com>
2022-07-12 20:18:56 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.getJSON("https://australia-southeast1-elevated-cargo-355220.cloudfunctions.net/backend-function", function(data){
-        $("#visitor-count").text(data.currentVisitor);
+        $("#visitor_count").text(data.currentVisitor);
         $('[data-toggle="counter-up"]').counterUp({
             delay: 10,
             time: 1000
