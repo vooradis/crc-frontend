35eec152056eb296f52efb4f38bd19acd6f44481
Samrat Vooradi <106648385+vooradis@users.noreply.github.com>
2022-07-13 14:37:29 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-$(document).ready(function(){
-    $.getJSON("https://australia-southeast1-elevated-cargo-355220.cloudfunctions.net/backend-function", function(data){
-        $("#visitor_count").text(data.currentVisitor);
-        $('[data-toggle="counter-up"]').counterUp({
-            delay: 10,
-            time: 1000
-        });
-    }).fail(function(){
-        console.log("An error has occurred while fetching the visitor count.");
-    });
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
 });
+
+const functionApi = 'https://australia-southeast1-elevated-cargo-355220.cloudfunctions.net/backend-function';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
+    return count;
+}
